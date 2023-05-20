01ba017547a60c4661b6d066ba9d01e175534352
tarikulsk <tarikulsk4@gmai.com>
2023-05-20 21:49:47 +0530

    auth Complete

--- src/Shared/Header/Header.jsx
+++ src/Shared/Header/Header.jsx
@@ -1,9 +1,29 @@
 // eslint-disable-next-line no-unused-vars
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
+import { AuthContext } from '../../Route/AuthProvider/AuthProvider';
 
 
 const Header = () => {
+
+
+    const { user, logOut } = useContext(AuthContext);
+    // console.log(user.displayName);
+    // console.log(user.photoURL);
+
+    const handleLogOut = () => {
+
+        logOut()
+            // eslint-disable-next-line no-unused-vars
+            .then(result => {
+
+            })
+            .catch(error => {
+                console.error(error)
+            })
+
+    }
+
     return (
         <nav>
             <div className="navbar grid grid-cols-3 text-white bg-blue-900 px-10 shadow-lg mb-1">
@@ -20,7 +40,12 @@ const Header = () => {
                 </div>
                 <div className='justify-end'>
                     <Link to={'/login'} >Login</Link>
+                    {
+                        user && <img className='h-8 w-8' title={user.displayName} src={user.photoURL} alt="" />
+                    }
+                    {user && <span className='text-pink-100'> <button onClick={handleLogOut}>Sign Out</button> </span>}
                 </div>
+                {/* <img src={user.photoURL} alt="" /> */}
 
             </div>
         </nav>
