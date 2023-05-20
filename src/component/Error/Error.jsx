092a6632cf6201c3290821a9d884bed140a043fc
tarikulsk <tarikulsk4@gmai.com>
2023-05-20 14:59:33 +0530

    404 page final

--- src/component/Error/Error.jsx
+++ src/component/Error/Error.jsx
@@ -1,9 +1,11 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Error = () => {
     return (
-        <div>
+        <div className='text-center'>
+            <Link className='btn bg-red-500 border-none mt-5 -mb-7 absolute text-center text-white' to={'/'}>Go to Home</Link>
             <img src="https://media.licdn.com/dms/image/C5612AQEPYce5KpNLyg/article-cover_image-shrink_720_1280/0/1551659700811?e=2147483647&v=beta&t=O9mBMiF-V12jCRJwaBNDWLKNL8cku2QSoCXtKP3vCHg" alt="" />
         </div>
     );
